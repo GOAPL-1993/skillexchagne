a0df610cff116f734095c6149ffc805a7929edcf
GOPAL <btoeww1993@gmail.com>
2020-10-19 21:49:29 +0800

    modified menu, catalog

--- resources/views/includes/menu.blade.php
+++ resources/views/includes/menu.blade.php
@@ -1,3 +1,4 @@
+<div class="container">
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <a class="navbar-brand" href="{{ url('/') }}">學習交換平台</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -9,7 +10,7 @@
       <!-- <li class="nav-item active">
         <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
       </li> -->
-      @if (Route::has('login'))
+      <!-- @if (Route::has('login'))
       @auth
       <li class="nav-item">
         <a class="nav-link" href="{{ url('/dashboard') }}">Dashboard</a>
@@ -27,41 +28,38 @@
       </li>
       @endif
       @endif
-      @endif
+      @endif -->
+      <li class="nav-item">
+        <a class="nav-link" href="{{ url('/') }}">關於我們</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="{{ url('/') }}">數據分析</a>
+      </li>
       <li class="nav-item">
         <a class="nav-link" href="{{ url('/') }}">發表文章</a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="{{ url('/') }}">聊天室</a>
       </li>
-      <li class="nav-item">
-        <a class="nav-link" href="{{ url('/') }}">數據分析</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="{{ url('/') }}">關於我們</a>
-      </li>
       <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
           會員中心
         </a>
         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          @if (Route::has('login'))
-          @auth
-          <a class="dropdown-item" href="logout">logout</a>
-          @else
-          <a class="dropdown-item" href="{{ url('/login') }}">Login</a>
-          @if (Route::has('register'))
-          <a class="dropdown-item" href="{{ url('/register') }}">Register</a>
-          @endif
-          @endif
-          @endif
           <a class="dropdown-item" href="#">個人資料</a>
           <a class="dropdown-item" href="#">我的貼文</a>
           <a class="dropdown-item" href="#">我的收藏</a>
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
           <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Something else here</a>
+          @if (Route::has('login'))
+          @auth
+          <a class="dropdown-item" href="logout">登出</a>
+          @else
+          <a class="dropdown-item" href="{{ url('/login') }}">登入</a>
+          @if (Route::has('register'))
+          <a class="dropdown-item" href="{{ url('/register') }}">註冊</a>
+          @endif
+          @endif
+          @endif
         </div>
       </li>
       <!-- <li class="nav-item">
@@ -71,3 +69,4 @@
 
   </div>
 </nav>
+</div>
