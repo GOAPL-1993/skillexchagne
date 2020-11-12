e38df626b5d17cd34d1d62224abd7197ddd4b57b
GOPAL <btoeww1993@gmail.com>
2020-11-12 16:52:08 +0800

    modified updatepost

--- resources/views/pages/updatepost.blade.php
+++ resources/views/pages/updatepost.blade.php
@@ -14,23 +14,8 @@
 
   @include('includes.menu')
   @foreach ($wannaupdateposts as $wannaupdatepost)
-  <div class="container">
-<<<<<<< HEAD
-    <form id="postForm" class="container was-validated" style="margin-left:20%" method="get" action="/updatedPost/">
-    @csrf
-    <div style="display:none">
-      <label for="postid"></label>
-      <textarea id="postid" name="postid" rows="1" value="postid">{{ $wannaupdatepost -> id }}</textarea>
-    </div>
-    <div style="display:none">
-      <label for="postUsername"></label>
-      <textarea id="postUsername" name="postUsername" rows="1" value="postUsername">{{ $wannaupdatepost -> post_username }}</textarea>
-    </div>
-    <div style="display:none">
-      <label for="postUserid"></label>
-      <textarea id="postUserid" name="postUserid" rows="1" value="postUserid">{{ $wannaupdatepost -> post_user_id }}</textarea>
-=======
-    <form id="postForm" class="container was-validated" method="get" action="/updatedPost/">
+  <div class="container" style="padding-top:65px ; padding-bottom:90px">
+    <!-- <form id="postForm" class="container was-validated" style="margin-left:20%" method="get" action="/updatedPost/">
       @csrf
       <div style="display:none">
         <label for="postid"></label>
@@ -42,79 +27,93 @@
       </div>
       <div style="display:none">
         <label for="postUserid"></label>
-        <textarea id="postUserid" name="postUserid" rows="1" value="postUserid">{{ $wannaupdatepost -> post_user_id }}</textarea>
->>>>>>> df3cd181ccdb06b7a58653532dd124f10dc5a3fb
-      </div>
-      <div class="form-group col-7">
-        <label for="postSort">類別</label>
-        <select class="form-control" id="postSort" name="postSort" required>
-          <option>交換技能</option>
-          <option>找老師</option>
-          <option>找學生</option>
-        </select>
-      </div>
-      <div class="form-group col-7">
-        <label for="postArea">地點</label>
-        <select class="form-control" id="postArea" name="postArea" required>
-          <option>新北市</option>
-          <option>台北市</option>
-          <option>基隆市</option>
-          <option>桃園市</option>
-          <option>新竹縣</option>
-          <option>新竹市</option>
-          <option>苗栗縣</option>
-          <option>台中市</option>
-          <option>彰化縣</option>
-          <option>雲林縣</option>
-          <option>南投縣</option>
-          <option>嘉義縣</option>
-          <option>嘉義市</option>
-          <option>台南市</option>
-          <option>高雄市</option>
-          <option>屏東縣</option>
-          <option>台東縣</option>
-          <option>花蓮縣</option>
-          <option>宜蘭縣</option>
-        </select>
-      </div>
-      <div class="form-group col-7">
-        <label for="postWannaTeach">擅長技能</label>
-        <textarea class="form-control" id="postWannaTeach" name="postWannaTeach" rows="1" required>{{ $wannaupdatepost -> wanna_teach }}</textarea>
-        <span style="color:red">@error('name'){{$wanna_teach}} @enderror</span>
-      </div>
-      <div class="form-group col-7">
-        <label for="postWannaLearn">想學習技能</label>
-        <textarea class="form-control" id="postWannaLearn" name="postWannaLearn" rows="1" required>{{ $wannaupdatepost -> wanna_learn }}</textarea>
-        <span style="color:red">@error('name'){{$wanna_teach}} @enderror</span>
-      </div>
-      <div class="form-group col-7">
-        <label for="postBody">交換內容及方式</label>
-        <textarea class="form-control" id="postBody" name="postBody" rows="3" required>{{ $wannaupdatepost -> body }}</textarea>
-        <span style="color:red">@error('name'){{$wanna_learn}} @enderror</span>
-      </div>
-      <div class="form-group col-7">
-        <label for="postCatalog">擅長技能標籤</label>
-        <select multiple class="form-control" id="postCatalog" name="postCatalog" required>
-          <option>英文</option>
-          <option>日文</option>
-          <option>韓文</option>
-          <option>德文</option>
-          <option>法文</option>
-          <option>西班牙文</option>
-          <option>運動</option>
-          <option>藝術</option>
-          <option>鋼琴</option>
-          <option>吉他</option>
-          <option>前端程式</option>
-          <option>後端程式</option>
-        </select>
-        <span style="color:red">@error('name'){{$catalog}} @enderror</span>
-      </div>
-      <div class="post-footer">
-        <button type="button" class="btn btn-danger" data-dismiss="modal" onclick="self.location.href='/index'">取消</button>
-        <button type=submit class="btn btn-secondary" value=ADD>修改</button>
-      </div>
-    </form>
+        <textarea id="postUserid" name="postUserid" rows="1" value="postUserid">{{ $wannaupdatepost -> post_user_id }}</textarea> -->
+    <div>
+      <form id="postForm" class="container was-validated" method="get" action="/updatedPost/" style="margin-left:20%">
+        @csrf
+        <div style="display:none">
+          <label for="postid"></label>
+          <textarea id="postid" name="postid" rows="1" value="postid">{{ $wannaupdatepost -> id }}</textarea>
+        </div>
+        <div style="display:none">
+          <label for="postUsername"></label>
+          <textarea id="postUsername" name="postUsername" rows="1" value="postUsername">{{ $wannaupdatepost -> post_username }}</textarea>
+        </div>
+        <div style="display:none">
+          <label for="postUserid"></label>
+          <textarea id="postUserid" name="postUserid" rows="1" value="postUserid">{{ $wannaupdatepost -> post_user_id }}</textarea>
+        </div>
+        <div class="form-group col-7">
+          <label for="postSort">類別</label>
+          <select class="form-control" id="postSort" name="postSort" required>
+            <option>交換技能</option>
+            <option>找老師</option>
+            <option>找學生</option>
+          </select>
+        </div>
+        <div class="form-group col-7">
+          <label for="postArea">地點</label>
+          <select class="form-control" id="postArea" name="postArea" required>
+            <option>新北市</option>
+            <option>台北市</option>
+            <option>基隆市</option>
+            <option>桃園市</option>
+            <option>新竹縣</option>
+            <option>新竹市</option>
+            <option>苗栗縣</option>
+            <option>台中市</option>
+            <option>彰化縣</option>
+            <option>雲林縣</option>
+            <option>南投縣</option>
+            <option>嘉義縣</option>
+            <option>嘉義市</option>
+            <option>台南市</option>
+            <option>高雄市</option>
+            <option>屏東縣</option>
+            <option>台東縣</option>
+            <option>花蓮縣</option>
+            <option>宜蘭縣</option>
+          </select>
+        </div>
+        <div class="form-group col-7">
+          <label for="postWannaTeach">擅長技能</label>
+          <textarea class="form-control" id="postWannaTeach" name="postWannaTeach" rows="1" required>{{ $wannaupdatepost -> wanna_teach }}</textarea>
+          <span style="color:red">@error('name'){{$wanna_teach}} @enderror</span>
+        </div>
+        <div class="form-group col-7">
+          <label for="postWannaLearn">想學習技能</label>
+          <textarea class="form-control" id="postWannaLearn" name="postWannaLearn" rows="1" required>{{ $wannaupdatepost -> wanna_learn }}</textarea>
+          <span style="color:red">@error('name'){{$wanna_teach}} @enderror</span>
+        </div>
+        <div class="form-group col-7">
+          <label for="postBody">交換內容及方式</label>
+          <textarea class="form-control" id="postBody" name="postBody" rows="3" required>{{ $wannaupdatepost -> body }}</textarea>
+          <span style="color:red">@error('name'){{$wanna_learn}} @enderror</span>
+        </div>
+        <div class="form-group col-7">
+          <label for="postCatalog">擅長技能標籤</label>
+          <select multiple class="form-control" id="postCatalog" name="postCatalog" required>
+            <option>英文</option>
+            <option>日文</option>
+            <option>韓文</option>
+            <option>德文</option>
+            <option>法文</option>
+            <option>西班牙文</option>
+            <option>運動</option>
+            <option>藝術</option>
+            <option>鋼琴</option>
+            <option>吉他</option>
+            <option>前端程式</option>
+            <option>後端程式</option>
+          </select>
+          <span style="color:red">@error('name'){{$catalog}} @enderror</span>
+        </div>
+        <div class="post-footer">
+          <button type="button" class="btn btn-danger" data-dismiss="modal" onclick="self.location.href='/index'">取消</button>
+          <button type=submit class="btn btn-secondary" value=ADD>修改</button>
+        </div>
+      </form>
+    </div>
   </div>
   @endforeach
 
